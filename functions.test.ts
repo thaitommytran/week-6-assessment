00adf395aaf99ef9c90fa5ecabf222fb61f45e05
Thai Tran <thai.tommy.tran@gmail.com>
2022-06-10 10:05:33 -0500

    part 3: unit tests completed

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,15 @@
-const {shuffleArray} = require('./utils')
+const { shuffleArray } = require("./utils");
 
-describe('shuffleArray should', () => {
-    // CODE HERE
-})
+describe("shuffleArray should", () => {
+  // CODE HERE
+  test("return an array", () => {
+    let testArr = [1, 2, 3];
+    expect(Array.isArray(shuffleArray(testArr))).toBe(true);
+  });
+
+  test("return an array of the same length", () => {
+    let testArr = [4, 5, 6];
+    let shuffledArr = shuffleArray(testArr);
+    expect(shuffledArr.length).toEqual(testArr.length);
+  })
+});
