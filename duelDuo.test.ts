404811349884df128a5dc043632bfe358f98514c
Thai Tran <thai.tommy.tran@gmail.com>
2022-06-10 10:57:58 -0500

    part 4: write automated tests completed

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -17,4 +17,21 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
-})
+});
+
+test('Draw button displays the div with id = "choices"', async () => {
+    await driver.findElement(By.id("draw")).click();
+    const botContainer = await driver.findElement(By.id("choices"));
+    const displayed = await botContainer.isDisplayed();
+    expect(displayed).toBeTruthy();
+    await driver.sleep(5000);
+});
+
+test('"Add to duo" button displays the div with id = "player-duo"', async () => {
+    await driver.findElement(By.id("draw")).click();
+    await driver.findElement(By.className("bot-btn")).click();
+    const playerDuo = await driver.findElement(By.id("player-duo"));
+    const displayed2 = await playerDuo.isDisplayed();
+    expect(displayed2).toBeTruthy();
+    await driver.sleep(5000);
+});
